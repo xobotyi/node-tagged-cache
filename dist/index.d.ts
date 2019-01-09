ac9d2b6c4016d2e56b58888878e3d2386d3f7581
xobotyi <xog3@yandex.ru>
2019-01-09 03:27:26 +0300

    no need in dist and types in the repo.

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import TaggedCache from "./TaggedCache";
-export { TaggedCache };
-export { default as timestamp, enableTimestampCache, disableTimestampCache, setTimestampCacheTTL, getTimestampCacheTTL, } from "./timestamp";
-declare const _default: TaggedCache;
-export default _default;
--- dist/timestamp.d.ts
+++
@@ -1,5 +0,0 @@
-export default function (): number;
-export declare function disableTimestampCache(): void;
-export declare function enableTimestampCache(): void;
-export declare function setTimestampCacheTTL(interval: number): void;
-export declare function getTimestampCacheTTL(): number;
